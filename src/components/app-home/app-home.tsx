ddeaf76fcef1dfd4172f4b9511dcae345ca53e49
Maro de Melo <marodemelo10@gmail.com>
2021-10-18 13:12:19 -0300

    implementando a funcionalidade de geolocation

--- src/components/app-home/app-home.tsx
+++ src/components/app-home/app-home.tsx
@@ -1,10 +1,20 @@
 import { Component, h } from '@stencil/core';
+import { SettingsData } from '../../services/settings-data';
+import { Geolocation } from "@capacitor/geolocation"
+
 
 @Component({
   tag: 'app-home',
   styleUrl: 'app-home.css',
 })
 export class AppHome {
+async componentDidLoad(){
+  let coordinates = await Geolocation.getCurrentPosition()
+  console.log(coordinates)
+  await SettingsData.setCoords(coordinates.coords.latitude, coordinates.coords.longitude)
+}
+
+
   render() {
     return [
       <ion-header>
@@ -12,35 +22,35 @@ export class AppHome {
           <ion-title>Weather</ion-title>
           <ion-buttons slot="end">
             <ion-button href="/settings" routerDirection="forward">
-              <ion-icon slot="icon-only" name="settings"/>
+              <ion-icon slot="icon-only" name="settings" />
             </ion-button>
           </ion-buttons>
         </ion-toolbar>
       </ion-header>,
 
       <ion-content class="ion-padding">
-       <div class="weather-display">
-         <h1>23.4</h1>
-         <p>Sunny</p>
-         <ion-card>
-           <ion-card-header>
-             <ion-card-subtitle>Adelaide</ion-card-subtitle>
-             <ion-card-title>Overview</ion-card-title>
-           </ion-card-header>
-           <ion-card-content>
-             <ion-grid>
-               <ion-row>
-                 <ion-col size="6">Min</ion-col>
-                 <ion-col size="6">12</ion-col>
-               </ion-row>
-               <ion-row>
-               <ion-col size="6">Max</ion-col>
-                 <ion-col size="6">26</ion-col>
-               </ion-row>
-             </ion-grid>
-           </ion-card-content>
-         </ion-card>
-       </div>
+        <div class="weather-display">
+          <h1>23.4</h1>
+          <p>Sunny</p>
+          <ion-card>
+            <ion-card-header>
+              <ion-card-subtitle>Adelaide</ion-card-subtitle>
+              <ion-card-title>Overview</ion-card-title>
+            </ion-card-header>
+            <ion-card-content>
+              <ion-grid>
+                <ion-row>
+                  <ion-col size="6">Min</ion-col>
+                  <ion-col size="6">12</ion-col>
+                </ion-row>
+                <ion-row>
+                  <ion-col size="6">Max</ion-col>
+                  <ion-col size="6">26</ion-col>
+                </ion-row>
+              </ion-grid>
+            </ion-card-content>
+          </ion-card>
+        </div>
       </ion-content>,
     ];
   }
--- src/services/storage.ts
+++ src/services/storage.ts
@@ -1,12 +1,38 @@
 const storage = window.localStorage
 
 export function set(key: string, value: any): Promise<void> {
-    return new Promise((resolve, reject)=> {
-        try{
+    return new Promise((resolve, reject) => {
+        try {
             storage && storage.setItem(key, JSON.stringify(value))
             resolve()
-        } catch (err){
+        } catch (err) {
             reject(`Coludnt store object ${err}`)
         }
     })
+}
+
+export function remove(key: string): Promise<void> {
+    return new Promise((resolve, reject) => {
+        try {
+            storage && storage.remove(key)
+            resolve()
+        } catch (err) {
+            reject(`Coludnt remove object ${err}`)
+        }
+    })
+}
+
+export function get(key: string): Promise<any> {
+    return new Promise((resolve, reject) => {
+        try {
+            if (storage) {
+                const item = storage.getItem(key)
+                resolve(JSON.parse(item))
+            }
+            resolve(undefined)
+        }         
+     catch (err) {
+        reject(`Coludnt get object ${err}`)
+    }
+})
 }
